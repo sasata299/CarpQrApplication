6b54fe4a98bf20d28c1a5efb06a42f80d381e6dd
sasata299 <karaage299@gmail.com>
2016-09-19 16:07:00 +0900

    Toolbarを作成して表示する

---
+++ app/src/main/java/com/example/sasata299/carpqrapplication/MyAsyncTask.java
@@ -0,0 +1,18 @@
+package com.example.sasata299.carpqrapplication;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by sasata299 on 16/09/19.
+ */
+public class MyAsyncTask extends AsyncTask<Void, Void, String> {
+
+    public MyAsyncTask() {
+        super();
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+        return null;
+    }
+}
