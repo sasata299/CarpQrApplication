2ca9552a7148f3d39fc78a4ada04c7cb2675df01
sasata299 <karaage299@gmail.com>
2016-09-19 18:02:25 +0900

    AsyncTaskを使って非同期処理と、処理後の描画を実装する

--- app/src/main/java/com/example/sasata299/carpqrapplication/MainActivity.java
+++ app/src/main/java/com/example/sasata299/carpqrapplication/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.sasata299.carpqrapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +16,19 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyAsyncTask task = new MyAsyncTask(MainActivity.this);
+                task.execute();
+            }
+        });
     }
 
-
+    public void setTextView(String res) {
+        TextView text = (TextView) findViewById(R.id.text);
+        text.setText(res);
+    }
 }
--- app/src/main/java/com/example/sasata299/carpqrapplication/MyAsyncTask.java
+++ app/src/main/java/com/example/sasata299/carpqrapplication/MyAsyncTask.java
@@ -1,18 +1,72 @@
 package com.example.sasata299.carpqrapplication;
 
+import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * Created by sasata299 on 16/09/19.
  */
 public class MyAsyncTask extends AsyncTask<Void, Void, String> {
 
-    public MyAsyncTask() {
+    OkHttpClient client = new OkHttpClient();
+    Context context;
+
+    public MyAsyncTask(Context context) {
         super();
+        this.context = context;
     }
 
     @Override
     protected String doInBackground(Void... params) {
-        return null;
+        String res = null;
+
+        try {
+            String result = run("http://api.openweathermap.org/data/2.5/weather?APPID=<apiKey>&q=Tokyo");
+            Log.i("show result", result);
+            JSONObject resJson = new JSONObject(result);
+            JSONArray weathers = resJson.getJSONArray("weather");
+            JSONObject weather = weathers.getJSONObject(0);
+            String description = weather.getString("description");
+            Log.i("show description", description);
+            res = description;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return res;
+    }
+
+    @Override
+    protected void onPostExecute(String res) {
+        super.onPostExecute(res);
+
+//        Toast.makeText(context, res, Toast.LENGTH_LONG).show();
+        MainActivity mainActivity = (MainActivity) context;
+        mainActivity.setTextView(res);
+    }
+
+    // OkHttpを使った処理
+    public String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().string();
     }
 }
