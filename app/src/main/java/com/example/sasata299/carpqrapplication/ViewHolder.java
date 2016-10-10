44830cff9b27c3ba6d13605a616824aa91621daa
sasata299 <karaage299@gmail.com>
2016-10-10 10:10:31 +0900

    BaseAdapterを使ってリストビュー表示する処理を追加する

---
+++ app/src/main/java/com/example/sasata299/carpqrapplication/ViewHolder.java
@@ -0,0 +1,14 @@
+package com.example.sasata299.carpqrapplication;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by sasata299 on 2016/10/10.
+ */
+
+public class ViewHolder {
+    ImageView iconView;
+    TextView inningView;
+    TextView detailView;
+}
