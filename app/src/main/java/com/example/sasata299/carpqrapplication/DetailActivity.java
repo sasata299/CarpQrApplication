8ea4163a314f06bf956c1b08fd991d2e3513b74b
sasata299 <karaage299@gmail.com>
2016-10-24 18:21:51 +0900

    詳細ページに遷移したときにスコアを表示するようにしてみる

--- app/src/main/java/com/example/sasata299/carpqrapplication/DetailActivity.java
+++ app/src/main/java/com/example/sasata299/carpqrapplication/DetailActivity.java
@@ -3,19 +3,26 @@ package com.example.sasata299.carpqrapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class DetailActivity extends AppCompatActivity {
+    @BindView(R.id.detailView) TextView detailView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        ButterKnife.bind(this);
+
         Intent intent = getIntent();
         if (intent != null) {
-            String str = intent.getStringExtra("sampleValue");
-            Toast.makeText(this, str, Toast.LENGTH_LONG).show();
+            detailView.setText(intent.getStringExtra("detail"));
+            Toast.makeText(this, "foobar", Toast.LENGTH_LONG).show();
         }
     }
 }
